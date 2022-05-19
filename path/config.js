6d43702ecfbc869c3a7b27b17c8ae7e077b71b31
Hirokazu Chiba <hchiba1@gmail.com>
2022-05-19 16:50:35 +0900

    Add path/config.js

---
+++ path/config.js
@@ -0,0 +1,15 @@
+{
+  node: {
+    caption: ['id'],
+    defaultIcon: false,
+    size: '@size',
+    color: '@color',
+  },
+  edge: {
+    caption: ['label'],
+    width: '@width',
+    color: '@color',
+    opacity: 0.6,
+  },
+  layout: 'default',
+}
