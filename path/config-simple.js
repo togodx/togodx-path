e02fc91d5f6777b7911a9db17a4e3bce27f2d2b7
Hirokazu Chiba <hchiba1@gmail.com>
2022-05-20 16:43:55 +0900

    Add simple version

---
+++ path/config-simple.js
@@ -0,0 +1,14 @@
+{
+  node: {
+    caption: ['displayLabel'],
+    defaultIcon: false,
+    color: '@color',
+  },
+  edge: {
+    caption: [],
+    width: 4,
+    color: '@color',
+    opacity: 0.6,
+  },
+  layout: 'default',
+}
