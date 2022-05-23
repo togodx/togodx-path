969e46e6c0436851f8f1f405fc2c5eb0343a519b
Hirokazu Chiba <hchiba1@gmail.com>
2022-05-23 16:39:19 +0900

    Modify to display_label in path/config.js

--- path/config.js
+++ path/config.js
@@ -1,11 +1,11 @@
 {
   node: {
-    caption: ['displayLabel'],
+    caption: ['display_label'],
     defaultIcon: false,
     color: '@color',
   },
   edge: {
-    caption: ['displayLabel'],
+    caption: ['display_label'],
     width: 4,
     color: '@color',
     opacity: 0.6,
