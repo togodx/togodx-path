0f7ddf60b7cd85892e040b8e19b31b51773fd51d
Hirokazu Chiba <hchiba1@gmail.com>
2022-06-14 11:39:04 +0900

    Refactor make_edge.pl

--- make_edge.pl
+++ make_edge.pl
@@ -10,30 +10,27 @@ my $USAGE=
 my %OPT;
 getopts('', \%OPT);
 
-my @path = `./js/togoid-route.js | sort -u`;
-chomp(@path);
-my %EDGE = ();
-my %NODE = ();
-for my $path (@path) {
-    my @f = split("\t", $path);
+my %ROUTE = ();
+my @ROUTE = `./js/togoid-route.js | sort -u`;
+chomp(@ROUTE);
+for my $route (@ROUTE) {
+    my @f = split("\t", $route);
     if (@f != 2) {
         die;
     }
     my ($source, $target) = @f;
-    $NODE{$source} = 1;
-    $NODE{$target} = 1;
-    $EDGE{"${source}-${target}"} = 1;
+    $ROUTE{$source}{$target} = 1;
 }
 
-my @togoid_config = `./js/list-togoid-config.js`;
-chomp(@togoid_config);
-for my $togoid_config (@togoid_config) {
-    my @f = split("-", $togoid_config);
+my @EDGE = `./js/list-togoid-config.js`;
+chomp(@EDGE);
+for my $EDGE (@EDGE) {
+    my @f = split("-", $EDGE);
     if (@f != 2) {
         die;
     }
     my ($source, $target) = @f;
-    if ($NODE{$source} && $NODE{$target} && $EDGE{$togoid_config}) {
+    if ($ROUTE{$source}{$target}) {
         print "$source -> $target\n";
     }
 }
