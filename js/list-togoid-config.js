1f71dc575740c47b51ae16a54ff6a13e9f876453
Hirokazu Chiba <hchiba1@gmail.com>
2022-06-14 10:45:32 +0900

    Add js/list-togoid-config.js --verbose option

--- js/list-togoid-config.js
+++ js/list-togoid-config.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
+const program = require('commander');
 const axios = require('axios');
 
+program
+  .option('-v, --verbose', 'verbose')
+  .parse(process.argv);
+
+let opts = program.opts();
+
 axios.get('https://api.github.com/repos/togoid/togoid-config/git/trees/main').then(res => {
+  if (opts.verbose) {
+    console.log(res.data);
+  }
   for (let dir of res.data.tree) {
     if (dir.path === 'config') {
       listConfigDir(dir.url);
