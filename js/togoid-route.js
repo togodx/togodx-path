c16d4f2a7661eff6aac7ee6aa1aaf8c8b4dfa429
Hirokazu Chiba <hchiba1@gmail.com>
2022-06-14 09:57:03 +0900

    Add js

---
+++ js/togoid-route.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const axios = require('axios');
+
+const targetDatasets = ['ncbigene', 'ensembl_gene', 'uniprot', 'pdb', 'chebi', 'chembl_compound', 'pubchem_compound', 'glytoucan', 'mondo', 'mesh', 'nando', 'hp', 'togovar'];
+
+targetDatasets.forEach((source) => {
+  targetDatasets.forEach((target) => {
+    if (source !== target) {
+      let api = `https://integbio.jp/togosite_dev/sparqlist/api/togoid_route?source=${source}&target=${target}`;
+      axios.get(api).then(res => {
+        console.log(api);
+        printPaths(res.data);
+      }).catch(err => {
+        console.error(`cannot open ${api}`);
+        process.exit(1);
+      });
+    }
+  });
+});
+
+function printPaths(paths) {
+  paths.forEach((path) => {
+    console.log(path);
+    for (let i = 0; i<path.length-1; i++) {
+      console.log(path[i] + '\t' + path[i+1]);
+    }
+  });
+}
