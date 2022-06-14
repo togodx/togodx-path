2fc964bed43f84eef1b9aae943ae5c3e66a700f2
Hirokazu Chiba <hchiba1@gmail.com>
2022-06-14 17:06:49 +0900

    Refactor bin/make_graph.pl

--- bin/make_graph.pl
+++ bin/make_graph.pl
@@ -10,6 +10,12 @@ my $USAGE=
 my %OPT;
 getopts('', \%OPT);
 
+my $TOGOID_ONTOLOGY_TMP = "node_label.tmp";
+my $TOGOID_CONFIG_TMP = "js/list-togoid-config.tmp";
+my $TOGOID_ROUTE_TMP = "js/togoid-route.tmp";
+my $TOGOID_ROUTE_JS = ".js/togoid-route.js";
+my $TOGOID_CONFIG_JS = "js/list-togoid-config.js";
+
 my %TARGET = (
     'ncbigene' => 1, 
     'ensembl_gene' => 1,
@@ -38,72 +44,17 @@ my %COLOR = (
     );
 
 my %DISPLAY_LABEL = ();
-my @TABLE = `curl -LSsf "https://docs.google.com/spreadsheets/d/16I2HJCpDBeoencNmzfW576q73LIciTMZOCrD7PjtXS4/export?format=tsv&gid=1295950655"`;
-chomp(@TABLE);
-for my $line (@TABLE) {
-    my @f = split("\t", $line);
-    my $source = $f[6];
-    my $target = $f[8];
-    my $display_label = $f[29];
-    $DISPLAY_LABEL{$source}{$target} = $display_label;
-}
-
+get_display_label();
 my %CATEGORY = ();
-my @CATEGORY = `curl -LSsf "https://docs.google.com/spreadsheets/d/16I2HJCpDBeoencNmzfW576q73LIciTMZOCrD7PjtXS4/export?format=tsv&gid=927983300"`;
-chomp(@CATEGORY);
-for my $line (@CATEGORY) {
-    my @f = split("\t", $line);
-    my $dataset = $f[0];
-    my $category = $f[1];
-    $CATEGORY{$dataset} = $category;
-    if ($category eq "Reaction") {
-        $CATEGORY{$dataset} = "Interaction";
-    }
-}
-
-if (!-s "node_label.tmp") {
-    system "curl -LSsf https://raw.githubusercontent.com/togoid/togoid-config/main/ontology/togoid-ontology.ttl > node_label.tmp";
-}
-open(ONTOLOGY, "node_label.tmp") || die "$!";
-my @ONTOLOGY = <ONTOLOGY>;
-chomp(@ONTOLOGY);
+get_category();
 my %CLASS_LABEL = ();
-my $CLASS;
-for my $line (@ONTOLOGY) {
-    if ($line =~ /^\S/) {
-        $CLASS = "";
-    } elsif ($line =~ /^ +dcterms:identifier +"(.+)" +;/) {
-        $CLASS = $1;
-    } elsif ($line =~ /^ +rdfs:label +"(.+)" +;/ && $CLASS) {
-        $CLASS_LABEL{$CLASS} = $1;
-    }
-}
-close(ONTOLOGY);
+get_class_label();
 
 my %NODE = ();
 my %ROUTE = ();
-if (!-f "js/togoid-route.tmp") {
-    if (-f "js/togoid-route.js") {
-        system "./js/togoid-route.js | sort -u > js/togoid-route.tmp";
-    } else {
-        die;
-    }
-}
-open(ROUTE, "js/togoid-route.tmp") || die "$!";
-my @ROUTE = <ROUTE>;
-chomp(@ROUTE);
-close(ROUTE);
-for my $route (@ROUTE) {
-    my @f = split("\t", $route);
-    if (@f != 2) {
-        die;
-    }
-    my ($source, $target) = @f;
-    $NODE{$source} = 1;
-    $NODE{$target} = 1;
-    $ROUTE{$source}{$target} = 1;
-}
+get_route_and_node();
 
+### Print nodes
 for my $node (sort keys %NODE) {
     my $category = $CATEGORY{$node} || die;
     print "$node\n";
@@ -115,19 +66,21 @@ for my $node (sort keys %NODE) {
     }
 }
 
-if (!-f "js/list-togoid-config.tmp") {
-    if (-f "js/list-togoid-config.js") {
-        system "./js/list-togoid-config.js > js/list-togoid-config.tmp";
+### Print edges
+if (!-f $TOGOID_CONFIG_TMP) {
+    if (-f $TOGOID_CONFIG_JS) {
+        system "$TOGOID_CONFIG_JS > $TOGOID_CONFIG_TMP";
     } else {
         die;
     }
 }
-open(LIST, "js/list-togoid-config.tmp") || die "$!";
+open(LIST, $TOGOID_CONFIG_TMP) || die "$!";
 my @EDGE = <LIST>;
 chomp(@EDGE);
 close(LIST);
-for my $EDGE (@EDGE) {
-    my @f = split("-", $EDGE);
+
+for my $edge (@EDGE) {
+    my @f = split("-", $edge);
     if (@f != 2) {
         die;
     }
@@ -138,5 +91,86 @@ for my $EDGE (@EDGE) {
         print "  link: \"$source-$target\"\n";
         print "  display_label: \"$DISPLAY_LABEL{$source}{$target}\"\n";
         print "  color: \"$COLOR{$category}\"\n";
+    }
+}
+
+################################################################################
+### Function ###################################################################
+################################################################################
+sub get_display_label {
+
+    my @table = `curl -LSsf "https://docs.google.com/spreadsheets/d/16I2HJCpDBeoencNmzfW576q73LIciTMZOCrD7PjtXS4/export?format=tsv&gid=1295950655"`;
+    chomp(@table);
+
+    for my $line (@table) {
+        my @f = split("\t", $line);
+        my $source = $f[6];
+        my $target = $f[8];
+        my $display_label = $f[29];
+        $DISPLAY_LABEL{$source}{$target} = $display_label;
+    }
+}
+
+sub get_category {
+
+    my @category = `curl -LSsf "https://docs.google.com/spreadsheets/d/16I2HJCpDBeoencNmzfW576q73LIciTMZOCrD7PjtXS4/export?format=tsv&gid=927983300"`;
+    chomp(@category);
+
+    for my $line (@category) {
+        my @f = split("\t", $line);
+        my $dataset = $f[0];
+        my $category = $f[1];
+        $CATEGORY{$dataset} = $category;
+        if ($category eq "Reaction") {
+            $CATEGORY{$dataset} = "Interaction";
+        }
+    }
+}
+
+sub get_class_label {
+
+    if (!-s $TOGOID_ONTOLOGY_TMP) {
+        system "curl -LSsf https://raw.githubusercontent.com/togoid/togoid-config/main/ontology/togoid-ontology.ttl > $TOGOID_ONTOLOGY_TMP";
+    }
+    open(ONTOLOGY, $TOGOID_ONTOLOGY_TMP) || die "$!";
+    my @ontology = <ONTOLOGY>;
+    chomp(@ontology);
+
+    my $class_id;
+    for my $line (@ontology) {
+        if ($line =~ /^\S/) {
+            $class_id = "";
+        } elsif ($line =~ /^ +dcterms:identifier +"(.+)" +;/) {
+            $class_id = $1;
+        } elsif ($line =~ /^ +rdfs:label +"(.+)" +;/ && $class_id) {
+            $CLASS_LABEL{$class_id} = $1;
+        }
+    }
+    close(ONTOLOGY);
+}
+
+sub get_route_and_node {
+
+    if (!-f $TOGOID_ROUTE_TMP) {
+        if (-f $TOGOID_ROUTE_JS) {
+            system "$TOGOID_ROUTE_JS | sort -u > $TOGOID_ROUTE_TMP";
+        } else {
+            die;
+        }
+    }
+    open(ROUTE, "$TOGOID_ROUTE_TMP") || die "$!";
+    my @ROUTE = <ROUTE>;
+    chomp(@ROUTE);
+    close(ROUTE);
+
+    for my $route (@ROUTE) {
+        my @f = split("\t", $route);
+        if (@f != 2) {
+            die;
+        }
+        my ($source, $target) = @f;
+        $NODE{$source} = 1;
+        $NODE{$target} = 1;
+        $ROUTE{$source}{$target} = 1;
     }
 }
