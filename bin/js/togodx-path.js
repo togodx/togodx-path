3609bfa011643ed384c7c84ea0c9a83305dea99d
Hirokazu Chiba <hchiba1@gmail.com>
2022-06-18 13:23:22 +0900

    Fix bin/js/togodx-path.js

--- bin/js/togodx-path.js
+++ bin/js/togodx-path.js
@@ -10,15 +10,16 @@ let opts = program.opts();
 const targetDatasets = ['ncbigene', 'ensembl_gene', 'uniprot', 'pdb', 'chebi', 'chembl_compound', 'pubchem_compound', 'glytoucan', 'mondo', 'mesh', 'nando', 'hp', 'togovar'];
 
 let out = {};
+targetDatasets.forEach((source) => {
+  out[source] = {};
+});
 let promises = [];
 targetDatasets.forEach((source) => {
   targetDatasets.forEach((target) => {
     if (source !== target) {
       let api = `https://integbio.jp/togosite_dev/sparqlist/api/togoid_route?source=${source}&target=${target}`;
       const promise = axios.get(api).then(res => {
-        const obj = {};
-        obj[target] = res.data;
-        out[source] = obj;
+        out[source][target] = res.data;
       }).catch(err => {
         console.error(err);
         process.exit(1);
@@ -30,4 +31,6 @@ targetDatasets.forEach((source) => {
 
 Promise.all(promises).then(() => {
   console.log(JSON.stringify(out, null, '  '));
+}).catch(err => {
+  console.error(err);
 });
