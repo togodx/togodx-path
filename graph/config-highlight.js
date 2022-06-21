154c579ed2d7fa623ccf2d9d1c5b486beb7b26e1
Hirokazu Chiba <hchiba1@gmail.com>
2022-06-21 17:26:09 +0900

    Add pathNodeList in graph/config-highlight.js

--- graph/config-highlight.js
+++ graph/config-highlight.js
@@ -7,19 +7,30 @@
     onClick: (n) => {
       blitzboard.pathSrc = n.id;
       pathSourceNode.textContent = `${n.id}`;
-      pathArrow.textContent = ' =>';
+      pathArrow.textContent = '--->';
       pathTargetNode.textContent = '';
+      while (pathNodeList.firstChild) {
+        pathNodeList.removeChild(pathNodeList.firstChild);
+      }
     },
     onHover: (n) => {
       if (blitzboard.pathSrc && pathList[blitzboard.pathSrc] && pathList[blitzboard.pathSrc][n.id]) {
         pathTargetNode.textContent = '' + n.id;
         let edgeIds = [];
+        while (pathNodeList.firstChild) {
+          pathNodeList.removeChild(pathNodeList.firstChild);
+        }
         pathList[blitzboard.pathSrc][n.id].forEach((path) => {
+          const child = document.createElement('div');
+          pathNodeList.appendChild(child);
+          child.textContent = `${blitzboard.pathSrc}`;
           for (let i = 0; i < path.length - 1; i++) {
             if (blitzboard.hasEdge(path[i], path[i+1])) {
               edgeIds.push(`${path[i]}-${path[i+1]}`);
+              child.textContent += ` -> ${path[i+1]}`;
             } else if (blitzboard.hasEdge(path[i+1], path[i])) {
               edgeIds.push(`${path[i+1]}-${path[i]}`);
+              child.textContent += ` <- ${path[i+1]}`;
             }
           }
         });
