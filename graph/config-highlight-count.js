4a7a70f32523b5664bae9d44280d2e97f56b77a0
Hirokazu Chiba <hchiba1@gmail.com>
2022-07-27 10:43:24 +0900

    Update graph/config-highlight-count.js to show pathCountStartEnd

--- graph/config-highlight-count.js
+++ graph/config-highlight-count.js
@@ -23,7 +23,7 @@
           const child = document.createElement('div');
           pathNodeList.appendChild(child);
           child.textContent = `${blitzboard.pathSrc}`;
-          child.textContent += ` (${datasetCount[blitzboard.pathSrc]})`;
+          child.textContent += ` (${pathCountStartEnd[blitzboard.pathSrc][n.id].start} / ${datasetCount[blitzboard.pathSrc]})`
           for (let i = 0; i < path.length - 1; i++) {
             // nodeSet.add(path[i+1]);
             if (blitzboard.hasEdge(path[i], path[i+1])) {
@@ -34,10 +34,11 @@
               edgeSet.add(`${path[i+1]}-${path[i]}`);
             }
             child.textContent += ` - ${path[i+1]}`;
-            if (pathCount[blitzboard.pathSrc][path[i+1]]) {
-              child.textContent += ` (${pathCount[blitzboard.pathSrc][path[i+1]]})`;
-            }
+            // if (pathCount[blitzboard.pathSrc][path[i+1]]) {
+            //   child.textContent += ` (${pathCount[blitzboard.pathSrc][path[i+1]]})`;
+            // }
           }
+          child.textContent += ` (${pathCountStartEnd[blitzboard.pathSrc][n.id].end} / ${datasetCount[n.id]})`;
         });
         blitzboard.network.setSelection({
           nodes: Array.from(nodeSet),
