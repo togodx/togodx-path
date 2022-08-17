a3cad05086f1d2e10efaaca6f9a6ef45221ccf79
Hirokazu Chiba <hchiba1@gmail.com>
2022-08-17 15:22:13 +0900

    Update bin/check_ids_in_paths.pl

--- bin/check_ids_in_paths.pl
+++ bin/check_ids_in_paths.pl
@@ -4,17 +4,37 @@ use File::Basename;
 use Getopt::Std;
 my $PROGRAM = basename $0;
 my $USAGE=
-"Usage: $PROGRAM DIR ID_TSV
+"Usage: $PROGRAM data/relation/output/
 ";
 
 my %OPT;
 getopts('', \%OPT);
 
-if (@ARGV != 2) {
+if (@ARGV != 1) {
     print STDERR $USAGE;
     exit 1;
 }
-my ($DIR, $ID_LIST) = @ARGV;
+my ($DIR) = @ARGV;
+
+my $DATASET_COUNT_FILE = "tsv/dataset-count-ids.tsv";
+my $ID_LIST = "tsv/ids.tsv";
+
+STDOUT->autoflush;
+
+# my %DATASET_COUNT = ();
+# open(DATASET_COUNT, "$DATASET_COUNT_FILE") || die;
+# while (<DATASET_COUNT>) {
+#     chomp;
+#     my @f = split;
+#     if (@f != 2) {
+#         die;
+#     }
+#     my ($dataset, $count) = @f;
+#     if ($count =~ /^\d+$/) {
+#         $DATASET_COUNT{$dataset} = $count;
+#     }
+# }
+# close(DATASET_COUNT);
 
 my %ID_LIST = ();
 open(ID_LIST, "$ID_LIST") || die;
@@ -31,10 +51,8 @@ close(ID_LIST);
 
 chdir($DIR) || die "$DIR: $!";
 
-# my @CSV = ("chebi-mesh.csv");
 my @CSV = `ls *.csv`;
 chomp(@CSV);
-my %NOT_FOUND = ();
 for my $csv (@CSV) {
     my ($dataset1, $dataset2);
     if ($csv =~ /^(\w+)-(\w+)\.csv$/) {
@@ -42,6 +60,9 @@ for my $csv (@CSV) {
     } else {
         die;
     }
+
+    my %found = ();
+    my %combinations = ();
     open(CSV, $csv) || die;
     while (<CSV>) {        
         chomp;
@@ -50,20 +71,21 @@ for my $csv (@CSV) {
             die;
         }
         my ($id1, $id2) = @f;
-        if (!$ID_LIST{$dataset1}{$id1}) {
-            $NOT_FOUND{$dataset1}{$id1} = 1;
-        }
-        if (!$ID_LIST{$dataset2}{$id2}) {
-            $NOT_FOUND{$dataset2}{$id2} = 1;
+        if ($ID_LIST{$dataset1}{$id1} && $ID_LIST{$dataset2}{$id2}) {
+            $found{$dataset1}{$id1} = 1;
+            $found{$dataset2}{$id2} = 1;
+            $combinations{"$id1\t$id2"} = 1;
         }
     }
     close(CSV);
-}
 
-for my $dataset (keys %NOT_FOUND) {
-    my $n = keys %{$NOT_FOUND{$dataset}};
-    print "$dataset\t$n\n";
-    # for my $id (keys %{$NOT_FOUND{$dataset}}) {
-    #     print "$dataset $id\n";
-    # }
+    print $dataset1;
+    print "\t";
+    print $dataset2;
+    print "\t", scalar(keys(%{$found{$dataset1}}));
+    # print "/", $DATASET_COUNT{$dataset1};
+    print "\t", scalar(keys(%{$found{$dataset2}}));
+    # print "/", $DATASET_COUNT{$dataset2};
+    print "\t", scalar(keys(%combinations));
+    print "\n";
 }
